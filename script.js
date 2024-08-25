5466e6ecd03995621a71fad5f982353f2b2e7e03
charmicoz <charmicoz1@gmail.com>
2024-08-25 18:44:42 +0300

    Test commit #1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 let progress_Main = 0.1;
-let progress_M1_1 = 1.5;  
+let progress_M1_1 = 25;  
 
 const progressElement_Main = document.querySelector('.progress-main');
 const progressElement_M1_1 = document.querySelector('.progress-m1-1');
