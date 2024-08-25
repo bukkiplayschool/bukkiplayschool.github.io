104673c48b62c61b13a96a052d2d2737ee235fa4
charmicoz <charmicoz1@gmail.com>
2024-08-25 18:41:29 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let progress_Main = 0.1;
+let progress_M1_1 = 1.5;  
+
+const progressElement_Main = document.querySelector('.progress-main');
+const progressElement_M1_1 = document.querySelector('.progress-m1-1');
+
+
+progressElement_Main.style.width = `${progress_Main}%`;
+progressElement_M1_1.style.width = `${progress_M1_1}%`;
